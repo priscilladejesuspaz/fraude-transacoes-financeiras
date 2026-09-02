f8d4b0a09052e1ed41795243beffa915b4ae7cb2
PRISCILLA DE J.PAZ <priscilladejesuspaz@gmail.com>
2026-09-02 17:58:34 -0300

    Delete sql directory

--- sql/02_import_data.sql
+++
@@ -1,9 +0,0 @@
-USE fraude_cartao;
--- Importando DATASET, pois pelo Wizard deu erro, devido o tamanho do mesmo.
-LOAD DATA LOCAL INFILE 'C:\Users\Book\Downloads\archive\test.csv'
-INTO TABLE transacoes
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
-IGNORE 1 ROWS;
-
